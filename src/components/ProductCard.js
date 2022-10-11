81ff8ebebead9e4df6fd5fdd63e6d5882768c3c4
Drew Osei <andrewowusu21@yahoo.com>
2022-10-11 11:09:03 +0000

    feat-show cart icon on product hover (#9)

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,12 +2,17 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCurrencySymbol } from "../store/selctors";
+import { ReactComponent as CartIcon } from "../assets/carticon.svg";
 import styles from "../styles/ProductCard.module.css";
 
 class ProductCard extends Component {
   constructor() {
     super();
   }
+
+  addProductToCartHandler = () => {
+    console.log("will add product to cart");
+  };
 
   render() {
     const { product, currencySymbol } = this.props;
@@ -19,12 +24,19 @@ class ProductCard extends Component {
     return (
       <div
         className={styles.container}
+        data-product__active={product.inStock}
         style={{ opacity: `${!product.inStock ? 0.35 : 1}` }}
       >
         <div className={styles["img-container"]}>
           <div className={styles["img-container--box"]} />
           <img src={product.gallery[0]} alt={product.name} />
           {!product.inStock && <h3>Out of stock</h3>}
+          <button
+            className={styles["product__cart-icon"]}
+            onClick={this.addProductToCartHandler}
+          >
+            <CartIcon fill="#fff" />
+          </button>
         </div>
         <div className="mt-2">
           <h1 className={styles["product--name"]}>{product.name}</h1>
